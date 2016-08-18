38bb20cf9bc160bf682a340338c38d5b9d1dfa7c
hest0001 <hest0001@outlook.com>
2016-08-18 17:55:07 +0800

    8/18

---
+++ 华为机试题/质数因子排序.java
@@ -0,0 +1,25 @@
+package 华为机试题;
+
+import java.util.*;
+
+public class 质数因子排序 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext())
+			System.out.println(getResult(sc.nextLong()));
+		sc.close();
+	}
+
+	public static String getResult(long num) {
+		int div = 2;
+		String result = "";
+		while (num != 1) {
+			while (num % div == 0) {
+				num = num / div;
+				result = result + div + " ";
+			}
+			div++;
+		}
+		return result;
+	}
+}
