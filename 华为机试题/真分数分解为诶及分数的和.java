42b8709fd4b9fd85a4afa52f6faffa4b727d1c4a
hest0001 <hest0001@outlook.com>
2016-08-24 16:30:28 +0800

    8/24

---
+++ 华为机试题/真分数分解为诶及分数的和.java
@@ -0,0 +1,52 @@
+package 华为机试题;
+
+import java.util.Scanner;
+
+public class 真分数分解为诶及分数的和 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			String str = sc.next();
+			Egyptsocre(str);
+		}
+		sc.close();
+
+	}
+
+	public static void Egyptsocre(String str) {
+		String[] parts = str.split("/");
+		// 分子
+		int a = Integer.parseInt(parts[0]);
+		// 分母
+		int b = Integer.parseInt(parts[1]);
+		StringBuilder sb = new StringBuilder();
+		int c;
+
+		while (a != 1) {
+			// 特例8/35=1/5+1/35类型的一步到位，循环结束
+			if (b % (a - 1) == 0) {
+				sb.append("1/").append(b / (a - 1)).append('+');
+				a = 1;
+			} else {
+				// 通用情况得到埃及分数的分子
+				c = b / a + 1;
+				sb.append("1/").append(c).append('+');
+
+				// 通分得到差
+				a = a * c - b;
+				b = c * b;
+
+				// 整除则循环结束
+				if (b % a == 0) {
+					b = b / a;
+					a = 1;
+				}
+			}
+		}
+
+		sb.append("1/").append(b);
+		System.out.println(sb.toString());
+	}
+
+}
---
+++ 华为机试题/真分数分解为诶及分数的和2.java
@@ -0,0 +1,55 @@
+package 华为机试题;
+
+import java.util.Scanner;
+
+public class 真分数分解为诶及分数的和2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			String str = sc.next();
+			Egyptsocre(str);
+		}
+		sc.close();
+	}
+
+	public static void Egyptsocre(String str) {
+		String[] parts = str.split("/");
+		// 分子
+		int a = Integer.parseInt(parts[0]);
+		// 分母
+		int b = Integer.parseInt(parts[1]);
+		StringBuilder sb = new StringBuilder();
+		int c;
+
+		while (true) {
+
+			// 一般性算法得到埃及分数的分子
+			if (b % a == 0) {
+				c = b / a;
+				a = 1;
+			} else {
+				c = b / a + 1;
+			}
+
+			// 循环结束判断
+			if (a == 1) {
+				sb.append("1/").append(c);
+				break;
+			} else if (a == 3 && b % 2 == 0) {
+				sb.append("1/").append(b / 2).append("+").append("1/").append(b);
+				break;
+			} else {
+				sb.append("1/").append(c).append("+");
+			}
+
+			// 通分a/b-1/c
+			a = a * c - b;
+			b = b * c;
+
+		}
+
+		System.out.println(sb.toString());
+	}
+
+}
